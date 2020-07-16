8e4063e249e1c1327dbd983fcf2f9d480e7e4d5c
WhyK <windchimeyk@gmail.com>
2020-07-16 13:06:45 +0900

    update: 環境を調整

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,6 @@
 import * as webpack from 'webpack';
 import * as path from 'path';
+import * as webpackDevServer from 'webpack-dev-server';
 
 const rules: webpack.RuleSetRule[] = [
   {
@@ -12,14 +13,23 @@ const module: webpack.Module = {
   rules
 };
 
+const devServer: webpackDevServer.Configuration = {
+  open: true,
+  openPage: 'dist/index.html',
+  watchContentBase: true,
+  port: 3000
+}
+
 const config: webpack.Configuration = {
   mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
+  watch: process.env.NODE_ENV === 'development' ? true : false,
   entry: 'src/assets/ts/index.ts',
   output: {
     filename: 'main.js',
-    path: path.resolve(`${process.cwd()}/dist/`)
+    path: path.resolve(process.cwd(), '/dist/')
   },
-  module
+  module,
+  devServer
 };
 
 export default config;
