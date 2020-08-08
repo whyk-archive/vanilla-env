dadd6c4267727a712f23c459043302d1f5e2e99b
WhyK <windchimeyk@gmail.com>
2020-08-08 23:42:02 +0900

    update: CSSの生成を調整

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,38 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const output = {
   filename: 'main.js',
   path: path.join(__dirname, 'dist/assets')
 }
+
+const rules = [
+  {
+    test: /\.scss/,
+    use: [
+      {
+        loader: MiniCssExtractPlugin.loader
+      },
+      {
+        loader: 'css-loader',
+        options: {
+          url: false
+        }
+      },
+      {
+        loader: 'sass-loader',
+        options: {
+          implementation: require('sass'),
+          sassOptions: {
+            sourceMap: true,
+            outputStyle: 'compressed',
+            fiber: require('fibers')
+          }
+        }
+      }
+    ]
+  }
+]
 
 const devServer = {
   open: true,
@@ -13,12 +42,22 @@ const devServer = {
   port: 3000
 }
 
+const plugins = [
+  new MiniCssExtractPlugin({
+    filename: 'main.css'
+  })
+]
+
 const config = {
   mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
   watch: process.env.NODE_ENV === 'development' ? true : false,
   entry: './src/assets/js/index.js',
   output,
-  devServer
+  module: {
+    rules
+  },
+  devServer,
+  plugins
 };
 
 module.exports = config
